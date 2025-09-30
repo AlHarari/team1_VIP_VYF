7cad21c24cab1195e628ac46e9c47b9d6fa5a5ee
AlHarari <hararia230@gmail.com>
2025-09-30 18:13:27 -0400

    Word list

--- corpus_stuff/visualize_corpus.py
+++ corpus_stuff/visualize_corpus.py
@@ -1,22 +1,16 @@
-####################################################
-## This script compares the word frequencies read ##
-## in by the model and how many space-separated   ##
-## strings there are.                             ##
-####################################################
+######################################################
+## This script produces the words_list.tsv file and ##
+## the vec_list.tsv file.                           ##
+######################################################
 import fasttext
 import numpy as np
 
-def vec2tsv_row(v: np.ndarray) -> str:
-    np.set_printoptions(floatmode="fixed", linewidth=500)
-    print(str(v))
-    return str(v) # Remove last bracket
-
 # Load in latest model.
-model_path = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_2947993/model_1/model" 
+model_path = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_3181969/model_0/model" 
 model = fasttext.load_model(model_path)
 
 with open("words_list.tsv", "w", encoding="utf-8") as text_dump_file, open("vectors_list.tsv", "w", encoding="utf-8") as vectors_dump_file:
     words_list = model.get_words()
-    for word in words_list[:100]:
+    for word in words_list:
         text_dump_file.write(word + "\n")
-        # vectors_dump_file.write(vec2tsv_row(model[word]) + "\n")
+        #np.savetxt("vectors_list.tsv", model[word].T, delimiter="\t")
