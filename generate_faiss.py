a71f791419af8e421cd517298f3d3a2bf49d5431
Mackenzie Mcquaig Warren <mwarren64@atl1-1-02-003-19-2.pace.gatech.edu>
2025-10-01 02:16:50 -0400

    working faiss index and saves to csv

--- generate_faiss.py
+++ generate_faiss.py
@@ -5,7 +5,7 @@ import csv
 import os
 
 # This model performed the best so far
-path_to_model = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_3181969/model_0/model"
+path_to_model = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_3181969/model_9/model"
 model = fasttext.load_model(path_to_model)
 input_matrix = model.get_input_matrix()
 words = model.get_words()
@@ -18,12 +18,12 @@ idx.add(input_matrix)
 
 # helper function to query nearest neighbors
 def get_neighbors(word, k):
-    if word not in words:
-        print(f"'{word}' not in corpus")
-        return []
-    word_idx = words.index(word)
-    query_vec = input_matrix[word_idx].reshape(1, cols)
-    sims, neighbors_idx = idx.search(query_vec, k)
+    #if word not in words:
+     #   print(f"'{word}' not in corpus")
+      #  return []
+    #word_idx = words.index(word)
+    query_vec = model[word]
+    sims, neighbors_idx = idx.search(query_vec.reshape((1, cols)), k)
     neighbors = [(words[i], sims[0][j]) for j, i in enumerate(neighbors_idx[0])]
     return neighbors
 
@@ -39,7 +39,7 @@ greek_words = [
 ]
 
 
-save_dir = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_3181969/model_0/model"
+save_dir = "/home/hice1/mwarren64/vip-folder"
 os.makedirs(save_dir, exist_ok=True)
 
 csv_path = os.path.join(save_dir, "greek_neighbors_1.csv")
@@ -51,9 +51,9 @@ with open(csv_path, "w", newline="", encoding="utf-8") as f:
         results_faiss = get_neighbors(qw, k=10)
         results_native = model.get_nearest_neighbors(qw, k=10)
         
-        if not results_faiss or not results_native:
-            print(f"warning: '{qw}' not in vocab")
-            continue
+        #if not results_faiss or not results_native:
+         #   print(f"warning: '{qw}' not in vocab")
+          #  continue
 
         for (score_f, neigh_f), (score_n, neigh_n) in zip(results_faiss, results_native):
             writer.writerow([qw, neigh_n, score_n, neigh_f, score_f])
