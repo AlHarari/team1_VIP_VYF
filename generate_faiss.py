493dcadde1f12d53658014715e2dd0465a0a6733
AlHarari <hararia230@gmail.com>
2025-10-22 13:45:58 -0400

    Fixed FAISS issue

--- generate_faiss.py
+++ generate_faiss.py
@@ -5,7 +5,7 @@ import csv
 import os
 
 # Set paths.
-common_path = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_3181969/model_9/"
+common_path = "/storage/ice-shared/vip-vyf/embeddings_team/models/array_models_3181969/model_9/" # Change to path of the model you're interested in.
 model_path = common_path + "model"
 matrix_path = common_path + "input_matrix_model9.npy"
 
@@ -13,6 +13,7 @@ matrix_path = common_path + "input_matrix_model9.npy"
 model = fasttext.load_model(model_path)
 words = model.get_words() # In the future, we'll probably just read the words_list.tsv file.
 input_matrix = np.load(matrix_path)
+input_matrix = input_matrix / np.linalg.norm(input_matrix, axis=1, keepdims=True)
 ROW, DIM = input_matrix.shape
 
 # Build the index.
@@ -21,9 +22,10 @@ idx.add(input_matrix)
 
 # helper function to query nearest neighbors
 def get_neighbors(word, k):
-    query_vec = model[word].reshape((1, DIM))
+    query_vec = model[word].reshape((1, DIM)) # This is why we can't query multiple vectors at one, unfortunately.
+    query_vec = query_vec / np.linalg.norm(query_vec, axis=1, keepdims=True)
     sims, neighbors_idx = idx.search(query_vec, k)
-    neighbors = [(words[i], sims[0][j]) for j, i in enumerate(neighbors_idx[0])] # In the future, it is possible that we'll query for multiple vectors at a time
+    neighbors = [(words[i], sims[0][j]) for j, i in enumerate(neighbors_idx[0])]
     return neighbors
 
 greek_words = [
@@ -40,7 +42,7 @@ greek_words = [
 save_dir = "./corpus_stuff"
 os.makedirs(save_dir, exist_ok=True)
 
-csv_path = os.path.join(save_dir, "greek_neighbors_1.csv")
+csv_path = os.path.join(save_dir, "greek_neighbors_normalized.csv")
 
 with open(csv_path, "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
